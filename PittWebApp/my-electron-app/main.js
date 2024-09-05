23fe4bef1950fb5b9daefb0b654ff7c9e2784108
Brett Noneman <brettn@vt.edu>
2024-09-05 05:24:28 -0400

    Simple Race Data Visualizer

---
+++ PittWebApp/my-electron-app/main.js
@@ -0,0 +1,28 @@
+// main.js
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false, // This is necessary to allow access to Node.js modules from the renderer process
+    }
+  });
+
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
